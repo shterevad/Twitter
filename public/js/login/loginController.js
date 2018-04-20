a4e290b2dc80265f75a9da40a21b0c3a62373d05
shterevad <desita_shtereva@abv.bg>
2018-04-20 21:59:53 +0300

    installed jquery / jquery-ui
    - installed jquery and jquery ui
    - animations at login page are working

--- public/js/login/loginController.js
+++ public/js/login/loginController.js
@@ -1,81 +1,39 @@
-loginApp.controller('LoginController', function($scope){
+$(function () {
 
-    $scope.signupClick = function($event){
-        $event.preventDefault();
-    }
-    // clicking Sign up:
-    // $("#login-pg-sign-btn").on('click', function($event){
-    //     $event.preventDefault();
-    //     $('body').css('overflow', 'hidden');
-    //     $("#login-right-side-container").hide("slide", {direction: 'right'}, 'slow', function(){
-    //         $("#login-right-side-container-signup-field").show("slide", {direction: 'right'}, 'slow'); 
-    //     });
-    // });
+    //clicking Sign up:
+    $("#login-pg-sign-btn").on('click', function(event){
+        event = event.originalEvent;
+        event.preventDefault();
+        $('body').css('overflow', 'hidden');
+        $("#login-right-side-container").hide("slide", {direction: 'right'}, 'slow', function(){
+            $("#login-right-side-container-signup-field").show("slide", {direction: 'right'}, 'slow'); 
+        });
+    });
 
-    // //clicking Log in:
-    // $("#login-pg-log-btn").on('click', function(event){
-    //     event = event.originalEvent;
-    //     event.preventDefault();
-    //     $('body').css('overflow', 'hidden');
-    //     $("#login-right-side-container").hide("slide", {direction: 'right'}, 'slow', function(){
-    //         $("#login-right-side-container-login-field").show("slide", {direction: 'right'}, 'slow'); 
-    //     });
-    // });
+    //clicking Log in:
+    $("#login-pg-log-btn").on('click', function(event){
+        event = event.originalEvent;
+        event.preventDefault();
+        $('body').css('overflow', 'hidden');
+        $("#login-right-side-container").hide("slide", {direction: 'right'}, 'slow', function(){
+            $("#login-right-side-container-login-field").show("slide", {direction: 'right'}, 'slow'); 
+        });
+    });
 
-    // //clicking Sign up from Log in form:
-    // $("#login-page-login-form-sign-btn").on('click', function(event){
-    //     event = event.originalEvent;
-    //     event.preventDefault();
-    //     $('body').css('overflow', 'hidden');
-    //     $("#login-right-side-container-login-field").hide("slide", {direction: 'right'}, 'slow', function(){
-    //         $("#login-right-side-container-signup-field").show("slide", {direction: 'right'}, 'slow'); 
-    //     });
-    // });
+    //clicking Sign up from Log in form:
+    $("#login-page-login-form-sign-btn").on('click', function(event){
+        event = event.originalEvent;
+        event.preventDefault();
+        $('body').css('overflow', 'hidden');
+        $("#login-right-side-container-login-field").hide("slide", {direction: 'right'}, 'slow', function(){
+            $("#login-right-side-container-signup-field").show("slide", {direction: 'right'}, 'slow'); 
+        });
+    });
     
-    // $(".footer-link>a").on('click', function(event){
-    //     event = event.originalEvent;
-    //     event.preventDefault();
+    $(".footer-link>a").on('click', function(event){
+        event = event.originalEvent;
+        event.preventDefault();
 
-    //     location.replace("/");
-    // })
+        location.replace("/");
+    })
 });
-
-// $(function () {
-
-//     //clicking Sign up:
-//     $("#login-pg-sign-btn").on('click', function(event){
-//         event = event.originalEvent;
-//         event.preventDefault();
-//         $('body').css('overflow', 'hidden');
-//         $("#login-right-side-container").hide("slide", {direction: 'right'}, 'slow', function(){
-//             $("#login-right-side-container-signup-field").show("slide", {direction: 'right'}, 'slow'); 
-//         });
-//     });
-
-//     //clicking Log in:
-//     $("#login-pg-log-btn").on('click', function(event){
-//         event = event.originalEvent;
-//         event.preventDefault();
-//         $('body').css('overflow', 'hidden');
-//         $("#login-right-side-container").hide("slide", {direction: 'right'}, 'slow', function(){
-//             $("#login-right-side-container-login-field").show("slide", {direction: 'right'}, 'slow'); 
-//         });
-//     });
-
-//     //clicking Sign up from Log in form:
-//     $("#login-page-login-form-sign-btn").on('click', function(event){
-//         event = event.originalEvent;
-//         event.preventDefault();
-//         $('body').css('overflow', 'hidden');
-//         $("#login-right-side-container-login-field").hide("slide", {direction: 'right'}, 'slow', function(){
-//             $("#login-right-side-container-signup-field").show("slide", {direction: 'right'}, 'slow'); 
-//         });
-//     });
-    
-//     $(".footer-link>a").on('click', function(event){
-//         event = event.originalEvent;
-//         event.preventDefault();
-
-//         location.replace("/");
-//     })
-// });
