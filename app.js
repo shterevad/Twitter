858a4952ec27a615d6e4ff7ab9c1082971e9d53b
shterevad <desita_shtereva@abv.bg>
2018-04-20 14:53:42 +0300

    connected mlab

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongodb = require('mongodb');
+var monk = require('monk');
+const MONGO_URL = 'mongodb://<shterevad>:<Spaghett1>@ds249079.mlab.com:49079/twitterittalents';
+var db = monk(MONGO_URL);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    req.db = db;
+  
+    next();
+  });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  var usersCollection = req.db.get('users');
+  console.log(usersCollection)
+  usersCollection.find({}, {}, function (err, docs) {
+    if (err) {
+      res.status(500);
+      res.json(err);
+    } else {
+      res.status(200);
+      res.json(docs);
+    }
+    console.log(res.status);
+  })
 });
 
 module.exports = router;
