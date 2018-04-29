8abd941a7e0abda2615324f2a1cac4ab6d2d8303
sokolovva <petyasokolovva@gmail.com>
2018-04-29 14:16:01 +0300

    tags and videos

--- public/js/routing.js
+++ public/js/routing.js
@@ -1,4 +1,4 @@
-var mainApp = angular.module("mainApp", ["ngRoute"/* , "infinite-scroll" */]);
+var mainApp = angular.module("mainApp", ["ngRoute", "ngSanitize"]);
 
 mainApp.config(function($routeProvider) {
     $routeProvider
