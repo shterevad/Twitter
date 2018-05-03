94d73d2ae5040ab67abe040e5f96bd9cb77fb5a0
shterevad <desita_shtereva@abv.bg>
2018-05-03 10:43:17 +0300

    added check to whotofollow buttons

--- public/directives/follow/followController.js
+++ public/directives/follow/followController.js
@@ -1,5 +1,17 @@
 mainApp.controller('followController', function ($scope, $http, userService) {
-    userService.getRandomUsers().then(users =>{
-        $scope.users=users;
-    })
+    userService.getRandomUsers().then(users => {
+        userService.getUserInSession()
+            .then(function (userInSession) {
+                $scope.users = [];
+                users.forEach(u => {
+                    if (userInSession.following.indexOf(u._id) >= 0) {
+                        u.followBack = true;
+                    } else {
+                        u.followBack = false;
+                    };
+                    $scope.users.push(u);
+                })
+            })
+            .catch(err => { console.log(err) })
+    }).catch(err => { console.log(err) })
 });
