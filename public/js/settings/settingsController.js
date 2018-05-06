0c8c51984ef0c3060a99be2016146588ed7e8762
shterevad <desita_shtereva@abv.bg>
2018-05-06 20:17:23 +0300

    user can now update his information via account settings
    - can also upload files

--- public/js/settings/settingsController.js
+++ public/js/settings/settingsController.js
@@ -3,30 +3,26 @@ mainApp.controller('settingsController', function ($scope, $window, $location, $
     $scope.settingsSection = 1;
     $scope.userInSettings = userService.getUserInSession();
     
-    // let config = {
-    //     apiKey: "AIzaSyDjF-wLRhxCVm2CKOG89G05-AzrYS6Ztog",
-    //     authDomain: "twitter-project-6abd2.firebaseapp.com",
-    //     databaseURL: "https://twitter-project-6abd2.firebaseio.com",
-    //     projectId: "twitter-project-6abd2",
-    //     storageBucket: "",
-    //     messagingSenderId: "827549512821"
-    //   };
-    // firebase.initializeApp(config);
-    // let storage = firebase.storage();
-    // let storageRef = firebase.storage.ref();
-
     $scope.saveProfileChanges = () => {
-        console.log($scope.userInSettings);
-        // console.log($scope.myFile)
+        // console.log($scope.userInSettings);
+        var updated = userService.updateUserFields({user : $scope.userInSettings});
+        // userService.updateUserFields($scope.userInSettings)
+        // .then(response => {
+        //     console.log(response);
+        // })
+        // .catch(err => console.log(err));
     }
 
     $scope.upload = ($event) => {
         let data = {}
         data.file = $event.target.files[0];
-        data.name = (+new Date()) + '-' + file.name;
-        data.metadata = { contentType: file.type };
-        userService.uploadPicture(data);
-        // console.log(document.querySelector('#account-profile-picture').files[0])
-        // $scope.userInSettings.profilePicture = $event.target.files[0];
+        data.name = (+new Date()) + '-' + data.file.name;
+        data.metadata = { contentType: data.file.type, auth : "Waffles" };
+        userService.uploadPicture(data)
+        .then(response => {
+            $scope.userInSettings[$event.target.name] = response;
+        })
+        .catch(err => console.log(err));
     }
+
 });
